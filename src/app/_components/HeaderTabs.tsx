91f9da5bb011e8688296125bfce8a3c523a290e7
hotteokButler <ygs12567@gmail.com>
2024-06-24 00:48:03 +0900

    Fix : 마이페이지 링크 노출  조건 추가

--- src/app/_components/HeaderTabs.tsx
+++ src/app/_components/HeaderTabs.tsx
@@ -14,7 +14,7 @@ export default function HeaderTabs() {
     <nav className={sty.headerTab_btn}>
       <Link href='/login'>로그인</Link>
       <Link href='/signup'>회원가입</Link>
-      {me.user_id && <Link href={`/users/${me.user_id}`}>마이페이지</Link>}
+      {(me.user_id && me.user_id !=='')  && <Link href={`/users/${me.user_id}`}>마이페이지</Link>}
     </nav>
   );
 }
