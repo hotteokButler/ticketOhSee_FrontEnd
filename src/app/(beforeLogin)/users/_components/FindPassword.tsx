9b176e207a69eb935ab74e06db019676a50cc4a4
hotteokButler <ygs12567@gmail.com>
2024-06-24 20:52:04 +0900

    Style : 아이디 및 비밀번호 찾기 UI - tab 버튼 및 help text

--- src/app/(beforeLogin)/users/_components/FindPassword.tsx
+++ src/app/(beforeLogin)/users/_components/FindPassword.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import sty from './findPage.module.css';
 import FindPageNavTab from './FindPageNavTab';
 
-export default function FindUser() {
+export default function FindPassword() {
   return (
     <div className={sty.find_wrap}>
       <FindPageNavTab />
-      <span className={sty.find_help}><sup>*</sup> 아이디를 찾을 방법을 선택해 주세요</span>
+      <span className={sty.find_help}><sup>*</sup> 비밀번호를 찾을 방법을 선택해 주세요</span>
+
     </div>
   );
 }
