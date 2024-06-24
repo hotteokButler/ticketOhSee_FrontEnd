d2ec47db8d1e0a7ce6d42f5290dc75b976e71795
hotteokButler <ygs12567@gmail.com>
2024-06-24 20:11:28 +0900

    Fix & Add : LoginNavBts 링크 수정 및 아이디 찾기 페이지 시작

--- src/app/(beforeLogin)/_components/LoginNavBtns.tsx
+++ src/app/(beforeLogin)/_components/LoginNavBtns.tsx
@@ -7,8 +7,8 @@ import sty from './login.module.css';
 export default function LoginNavBtns() {
   return (
     <nav className={sty.login_navBtn_wrap}>
-      <Link href="/user/find-user">아이디 찾기</Link>
-      <Link href="/user/find-password">비밀번호 찾기</Link>
+      <Link href="/users/find-user">아이디 찾기</Link>
+      <Link href="/users/find-password">비밀번호 찾기</Link>
       <Link href="/signup">회원가입</Link>
     </nav>
   )
