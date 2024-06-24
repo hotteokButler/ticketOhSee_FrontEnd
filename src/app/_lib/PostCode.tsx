921ef090b3629350833aa7e8567abbe95a53a3bc
hotteokButler <ygs12567@gmail.com>
2024-06-24 13:38:57 +0900

    Fix :주소 검색 시 참고 항목 조합 추가

--- src/app/_lib/PostCode.tsx
+++ src/app/_lib/PostCode.tsx
@@ -18,8 +18,8 @@ export default function PostCode({ user_address, setUserAddress, onClickEvent }:
     // 각 주소의 노출 규칙에 따라 주소를 조합한다.
     // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
     let addr = ''; // 주소 변수
+    let extraAddr = ''; //참고 항목 주소
     const zcode = data.zonecode; // 우편번호
-
 
     //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
     if (data.userSelectedType === 'R') {
@@ -30,7 +30,18 @@ export default function PostCode({ user_address, setUserAddress, onClickEvent }:
       addr = data.jibunAddress;
     }
 
-    setUserAddress(`[${zcode}] ${addr}`); // setAddress를 호출하여 부모 컴포넌트의 상태를 업데이트
+    // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+    if (data.userSelectedType === 'R') {
+      // 건물명이 있고, 공동주택일 경우 추가한다.
+      if (data.buildingName !== '' && data.apartment === 'Y') {
+        extraAddr += extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
+      }
+      // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+      if (extraAddr !== '') {
+        extraAddr = ' (' + extraAddr + ')';
+      }
+    }
+    setUserAddress(`[${zcode}] ${addr} ${extraAddr}`); // setAddress를 호출하여 부모 컴포넌트의 상태를 업데이트
   };
 
   const handleClick = () => {
