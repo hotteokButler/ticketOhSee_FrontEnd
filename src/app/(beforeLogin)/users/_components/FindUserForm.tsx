5ee8ca0783c287e1a5e024b402c688a201329dea
hotteokButler <ygs12567@gmail.com>
2024-06-25 20:22:40 +0900

    Add : FindUserForm 마무리
    - 아이디 찾기 Form 마무리 (email,phone따라 다르게 노출)
    - Ui 마무리

---
+++ src/app/(beforeLogin)/users/_components/FindUserForm.tsx
@@ -0,0 +1,140 @@
+'use client';
+
+import React, { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import sty from './findPage.module.css';
+import { IoIosArrowDown } from 'react-icons/io';
+import classNames from 'classnames/bind';
+import ErrorMessage from '../../_components/ErrorMessage';
+
+interface IFindUserForm {
+  user_name: string;
+  email?: string;
+  phone?: number | string;
+}
+
+type IProp = {
+  findUserWith: 'email' | 'phone';
+};
+
+const cx = classNames.bind(sty);
+
+export default function FindUserForm({ findUserWith }: IProp) {
+  const [toggle, setToggle] = useState(true);
+  let subtit = '';
+
+  switch (findUserWith) {
+    case 'email':
+      subtit = '등록된 이메일로 찾기';
+      break;
+
+    case 'phone':
+      subtit = '등록된 휴대폰번호로 찾기';
+      break;
+
+    default:
+      subtit = '등록된 이메일로 찾기';
+      break;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<IFindUserForm>({ mode: 'all' });
+
+  const handleToggleForm: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    setToggle((prev) => !prev);
+  };
+
+  const onSubmit: SubmitHandler<IFindUserForm> = async (data) => {
+    const { user_name, email, phone } = data;
+
+    let new_data: IFindUserForm = {
+      user_name: user_name.trim(),
+    };
+
+    if (email) {
+      new_data = {
+        ...new_data,
+        email: email.trim(),
+      };
+    }
+    if (phone) {
+      new_data = {
+        ...new_data,
+        phone: typeof phone === 'string' ? phone.trim() : phone,
+      };
+    }
+    console.log(new_data);
+
+    reset();
+  };
+  return (
+    <div className={sty.find_form_wrap}>
+      <button type='button' className={cx('find_form_toggle', { active: toggle })} onClick={handleToggleForm}>
+        <span>{subtit}</span> <IoIosArrowDown />
+      </button>
+      <div className={cx('find_form', { active: toggle })}>
+        <form name={`w_${findUserWith}`} onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type='text'
+            placeholder='이름'
+            {...register('user_name', {
+              required: '필수 입력 값입니다.',
+              pattern: {
+                value: /^[ㄱ-ㅎ가-힣a-zA-Z]*$/,
+                message: '한글 또는 영문으로 입력해주세요',
+              },
+            })}
+          />
+          {errors.user_name?.message && <ErrorMessage message={String(errors.user_name?.message)} />}
+
+          {findUserWith == 'email' && (
+            <input
+              type='text'
+              placeholder='이메일'
+              {...register('email', {
+                required: '필수 입력 값입니다.',
+                validate: {
+                  emailSelected: (val) => {
+                    const regexWithConfig = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i;
+                    let test = val ? regexWithConfig.test(val) : false;
+                    return test ? true : '이메일이 형식에 맞지 않습니다.';
+                  },
+                },
+              })}
+            />
+          )}
+          {errors?.email?.message && findUserWith == 'email' && (
+            <ErrorMessage message={String(errors?.email?.message)} />
+          )}
+
+          {findUserWith == 'phone' && (
+            <input
+              type='text'
+              placeholder='등록된 휴대폰번호 (-없이 입력)'
+              {...register('phone', {
+                required: '필수 입력 값입니다.',
+                pattern: {
+                  value: /^([0-9]{3})([0-9]{3,4})([0-9]{4})$/,
+                  message: '하이픈(-)을 빼고 전화번호를 입력해주세요',
+                },
+              })}
+            />
+          )}
+          {errors?.phone?.message && findUserWith == 'phone' && (
+            <ErrorMessage message={String(errors?.phone?.message)} />
+          )}
+
+          <button className={sty.find_form_submit} type='submit' disabled={!isValid}>
+            확인
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
