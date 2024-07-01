cfe0b989e8813e80a0005249c356dad09c14e8c1
hotteokButler <ygs12567@gmail.com>
2024-07-01 12:59:32 +0900

    Add : footer UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from './_components/Footer';
 import Header from './_components/Header';
 import MainArticle from './_components/MainArticle';
 import MainArticleBtn from './_components/MainArticleBtn';
@@ -7,19 +8,22 @@ import sty from './_components/main.module.css';
 
 export default function Home() {
   return (
-    <div className={sty.main_wrapper}>
-      <Header />
-      <MainSlide />
-      {/* ticket open */}
-      <MainArticle sub_title='TICKET OPEN'>
-        <MainArticleSlide />
-        <MainArticleBtn mf='upcomming' btn_txt='오픈예정 전체보기'/>
-      </MainArticle>
-      {/* popular list */}
-      <MainArticle sub_title='Popular List'>
-        <MainArticleSlide />
-        <MainArticleBtn mf='popular' btn_txt='인기 공연 전체보기'/>
-      </MainArticle>
-    </div>
+    <>
+      <div className={sty.main_wrapper}>
+        <Header />
+        <MainSlide />
+        {/* ticket open */}
+        <MainArticle sub_title='TICKET OPEN'>
+          <MainArticleSlide />
+          <MainArticleBtn mf='upcomming' btn_txt='오픈예정 전체보기' />
+        </MainArticle>
+        {/* popular list */}
+        <MainArticle sub_title='Popular List'>
+          <MainArticleSlide />
+          <MainArticleBtn mf='popular' btn_txt='인기 공연 전체보기' />
+        </MainArticle>
+      </div>
+      <Footer />
+    </>
   );
 }
