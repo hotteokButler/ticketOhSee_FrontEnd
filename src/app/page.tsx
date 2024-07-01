cd3c345521c683e8c05317bc58342b8612bb8821
hotteokButler <ygs12567@gmail.com>
2024-07-01 11:49:30 +0900

    Add : MainArticle sl 버튼

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from './_components/Header';
 import MainArticle from './_components/MainArticle';
+import MainArticleBtn from './_components/MainArticleBtn';
 import MainArticleSlide from './_components/MainArticleSlide';
 import MainSlide from './_components/MainSlide';
 import sty from './_components/main.module.css';
@@ -12,10 +13,12 @@ export default function Home() {
       {/* ticket open */}
       <MainArticle sub_title='TICKET OPEN'>
         <MainArticleSlide />
+        <MainArticleBtn mf='upcomming' btn_txt='오픈예정 전체보기'/>
       </MainArticle>
       {/* popular list */}
       <MainArticle sub_title='Popular List'>
         <MainArticleSlide />
+        <MainArticleBtn mf='popular' btn_txt='인기 공연 전체보기'/>
       </MainArticle>
     </div>
   );
