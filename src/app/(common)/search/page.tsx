b91652a1c291791201455593429faf232a5140a0
hotteokButler <ygs12567@gmail.com>
2024-07-02 23:00:11 +0900

    Add : Search page 시작₩

---
+++ src/app/(common)/search/page.tsx
@@ -0,0 +1,16 @@
+import Footer from '../../_components/Footer';
+import Header from '../../_components/Header';
+import sty from '../../_components/searchPage.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className={sty.main_search_wrapper}>
+        <Header />
+
+
+      </div>
+      <Footer />
+    </>
+  );
+}
