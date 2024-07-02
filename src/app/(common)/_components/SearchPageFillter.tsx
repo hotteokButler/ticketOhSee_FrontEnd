23948d9bbdebf4f5da773c94743758cbc2e6d180
hotteokButler <ygs12567@gmail.com>
2024-07-02 23:30:57 +0900

    Style :footer style 수정 및 search page 시작

---
+++ src/app/(common)/_components/SearchPageFillter.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import sty from './searchPage.module.css';
+
+export default function SearchPageFillter() {
+  return <aside className={sty.search_fillter_wrap}>
+
+  </aside>;
+}
