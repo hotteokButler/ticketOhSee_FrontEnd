d62fd2f41c1e348a50ad0da4f2774d13efe40370
hotteokButler <ygs12567@gmail.com>
2024-07-03 21:00:53 +0900

    Fix : data 구분 위한 field 구분

--- src/app/(common)/_components/SearchPageFillter.tsx
+++ src/app/(common)/_components/SearchPageFillter.tsx
@@ -6,10 +6,11 @@ import CalendarModule from './CalendarModule';
 import classNames from 'classnames/bind';
 
 type IFillterCatData = {
-  id: number;
-  title: string;
-  isMultiSelect: boolean;
-  FillterBtns: string[] | [];
+  id: number,
+  title: string,
+  ariaId : string,
+  isMultiSelect: boolean,
+  FillterBtns: string[] | []
 };
 
 const cx = classNames.bind(sty);
@@ -19,24 +20,28 @@ export default function SearchPageFillter() {
     {
       id: 1,
       title: '장르',
+      ariaId : 'genre',
       isMultiSelect: true,
       FillterBtns: ['연극', '뮤지컬', '콘서트', '아동/가족', '클래식/무용', '전시/행사'],
     },
     {
       id: 2,
       title: '판매상태',
+      ariaId: 'state',
       isMultiSelect: false,
       FillterBtns: ['판매중', '판매종료'],
     },
     {
       id: 3,
       title: '날짜',
+      ariaId: 'date',
       isMultiSelect: false,
       FillterBtns: [],
     },
     {
       id: 4,
-      title: '',
+      title: '지역',
+      ariaId: 'loaction',
       isMultiSelect: true,
       FillterBtns: [
         '서울',
@@ -63,17 +68,17 @@ export default function SearchPageFillter() {
         <h4>필터</h4>
 
         {fillterCatData &&
-          fillterCatData.map(({ id, title, isMultiSelect, FillterBtns }) => {
+          fillterCatData.map(({ id, title, ariaId,isMultiSelect, FillterBtns }) => {
             if (title !== '날짜') {
               // 날짜 카테고리의 경우 캘린더 import
               return (
-                <div className={sty.search_input_con} key={id}>
-                  <h5>
+                <div className={sty.search_input_con} key={id} >
+                  <h5 >
                     {title}
                     {isMultiSelect && <span className={sty.search_input_sub}>&#40;중복 선택 가능&#41;</span>}
                   </h5>
                   {FillterBtns.length > 0 && (
-                    <menu className={cx({ search_multiple_select: isMultiSelect })}>
+                    <menu className={cx({ search_multiple_select: isMultiSelect })} data-wrap={ariaId}>
                       {FillterBtns.map((btns, idx) => (
                         <button type='button' name={btns} key={idx}>
                           {btns}
@@ -85,9 +90,9 @@ export default function SearchPageFillter() {
               );
             } else {
               return (
-                <div className={sty.search_input_con}>
+                <div className={sty.search_input_con}  key={id} >
                   <h5>{title}</h5>
-                  <div className={sty.search_calender}>
+                  <div className={sty.search_calender} data-wrap={ariaId}>
                     <CalendarModule />
                   </div>
                 </div>
