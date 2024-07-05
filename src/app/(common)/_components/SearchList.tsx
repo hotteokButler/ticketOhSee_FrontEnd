93084a1f428bf42ecd4a5181a8de09fa63c918a2
hotteokButler <ygs12567@gmail.com>
2024-07-06 00:05:17 +0900

    Style : Search List UI

--- src/app/(common)/_components/SearchList.tsx
+++ src/app/(common)/_components/SearchList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import sty from './searchPage.module.css';
 import TicketList from './TicketList';
 import { faker } from '@faker-js/faker';
@@ -10,78 +10,91 @@ type ISearchSortFillter = {
   setSort: () => void;
 };
 
+export type ITicketData = {
+  id: number;
+  title: string;
+  place: string;
+  ticketImgUrl?: string;
+  period_start: string | Date;
+  period_end: string | Date;
+  is_liked?: boolean;
+  star: number;
+  review: number;
+};
+
 export default function SearchList() {
   const [sort, setSort] = useState<ISearchSortFillter>();
+  const [listData, setListData] = useState<ITicketData[]>();
 
-  const listData = [
-    {
-      id: 1,
-      title: '타이틀',
-      place: '공연장소',
-      ticketImgUrl: faker.image.urlPicsumPhotos({ width: 280 }),
-      is_liked : true,
-      period_start: new Date(),
-      period_end: new Date(),
-      star: 9.8,
-      review: 980,
-    },
-    {
-      id: 2,
-      title: '타이틀',
-      place: '공연장소',
-      ticketImgUrl: faker.image.urlPicsumPhotos({ width: 280 }),
-      period_start: new Date(),
-      period_end: new Date(),
-      star: 9.8,
-      review: 980,
-    },
-    {
-      id: 3,
-      title: '타이틀',
-      place: '공연장소',
-      ticketImgUrl: faker.image.urlPicsumPhotos({ width: 280 }),
-      period_start: new Date(),
-      period_end: new Date(),
-      star: 9.8,
-      review: 980,
-    },
-    {
-      id: 4,
-      title: '타이틀',
-      place: '공연장소',
-      ticketImgUrl: faker.image.urlPicsumPhotos({ width: 280 }),
-      period_start: new Date(),
-      period_end: new Date(),
-      star: 9.8,
-      review: 980,
-    },
-  ];
+  const handleSortBtn: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    const { name } = e.target as HTMLButtonElement;
+    console.log(name);
+  };
 
-  if (!listData) return;
+  useEffect(() => {
+    setListData((p) => [
+      {
+        id: 1,
+        title: '타이틀',
+        place: '공연장소',
+        is_liked: true,
+        period_start: new Date(),
+        period_end: new Date(),
+        star: 9.8,
+        review: 980,
+      },
+      {
+        id: 2,
+        title: '타이틀',
+        place: '공연장소',
+        period_start: new Date(),
+        period_end: new Date(),
+        star: 9.8,
+        review: 980,
+      },
+      {
+        id: 3,
+        title: '타이틀',
+        place: '공연장소',
+        period_start: new Date(),
+        period_end: new Date(),
+        star: 9.8,
+        review: 980,
+      },
+      {
+        id: 4,
+        title: '타이틀',
+        place: '공연장소',
+        period_start: new Date(),
+        period_end: new Date(),
+        star: 9.8,
+        review: 980,
+      },
+    ]);
+  }, []);
 
   return (
-    <div aria-label='티켓 리스트'>
+    <div className={sty.search_list_wrap} aria-label='티켓 리스트'>
       <div className={sty.search_list_top}>
         <h3>
-          티켓<span className={sty.search_list_total}>&#40;{listData.length}&#41;</span>
+          티켓<span className={sty.search_list_total}>&#40;{listData && listData.length}&#41;</span>
         </h3>
         <menu className={sty.search_sort_btn}>
-          <button type='button' data-id='more_correct'>
+          <button type='button' name='more_correct' onClick={handleSortBtn}>
             정확도순
           </button>
-          <button type='button' data-id='immediate'>
+          <button type='button' name='immediate' onClick={handleSortBtn}>
             공연임박순
           </button>
-          <button type='button' data-id='hit'>
+          <button type='button' name='hit' onClick={handleSortBtn}>
             판매많은순
           </button>
         </menu>
       </div>
 
       <div className={sty.search_list_con} aria-label='티켓 리스트'>
-        {listData.map((ticket) => (
-          <TicketList key={ticket.id} listData={ticket} />
-        ))}
+        {listData && listData.map((ticket) => <TicketList key={ticket.id} listData={ticket} />)}
+        {listData && listData.map((ticket) => <TicketList key={ticket.id} listData={ticket} />)}
       </div>
     </div>
   );
--- src/app/(common)/_components/TicketList.tsx
+++ src/app/(common)/_components/TicketList.tsx
@@ -3,19 +3,12 @@ import sty from './ticketList.module.css';
 import Link from 'next/link';
 import { FaRegHeart, FaHeart, FaStar } from 'react-icons/fa';
 import dayjs from 'dayjs';
+import { ITicketData } from './SearchList';
+import NoImage from './NoImage';
+
 
 type ITicketList = {
-  listData: {
-    id: number;
-    title: string;
-    place: string;
-    ticketImgUrl: string;
-    period_start: string | Date;
-    period_end: string | Date;
-    is_liked?: boolean;
-    star: number;
-    review: number;
-  };
+  listData: ITicketData
 };
 
 export default function TicketList({
@@ -23,20 +16,19 @@ export default function TicketList({
 }: ITicketList) {
   return (
     <div className={sty.ticket_wrap}>
-      <Link href='#'>
+      <Link href='#' scroll={false}>
         <div className={sty.ticket_img}>
-          <img src={ticketImgUrl} alt={title} />
+          {ticketImgUrl ?  <img src={ticketImgUrl} alt={title} /> : <NoImage/>}
         </div>
-
         <h5 className={sty.ticket_tit}>
           <span>{title}</span>
           {is_liked ? <FaHeart /> : <FaRegHeart />}
         </h5>
         <p className={sty.ticket_place}>{place}</p>
-        <p className={sty.ticket_period}>{`${dayjs(period_start).format('YYYY년MM월DD일')}~${dayjs(period_end).format('YYYY년MM월DD일')}`}</p>
+        <p className={sty.ticket_period}>{`${dayjs(period_start).format('YYYY\/MM\/DD')} ~ ${dayjs(period_end).format('YYYY년\/MM\/DD')}`}</p>
         <p className={sty.ticket_review}>
           <FaStar />
-          <span>{star}</span>
+          <span>&#40;{star}&#41;</span>
           <span>{`리뷰 ${review}`}</span>
         </p>
       </Link>
