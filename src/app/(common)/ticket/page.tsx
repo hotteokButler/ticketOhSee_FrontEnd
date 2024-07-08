989e02a376b620dba77d0b68ea0bb6fe7606548c
hotteokButler <ygs12567@gmail.com>
2024-07-08 23:13:14 +0900

    Add : ticketDetail page

--- src/app/(common)/ticket/page.tsx
+++ src/app/(common)/ticket/page.tsx
@@ -1,6 +1,6 @@
 import Footer from '../../_components/Footer';
 import Header from '../../_components/Header';
-import sty from '../_components/searchPage.module.css'
+import sty from '../_components/ticketDetail.module.css'
 
 
 export default function TicketPage() {
