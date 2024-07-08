e0dbc08305bec9aac3e15ac985438174a11bcb04
hotteokButler <ygs12567@gmail.com>
2024-07-08 23:10:47 +0900

    Rename & Add : search page component 이름 변경 및 ticket page  추가 시작

--- src/app/(common)/ticket/page.tsx
+++ src/app/(common)/ticket/page.tsx
@@ -1,19 +1,14 @@
 import Footer from '../../_components/Footer';
 import Header from '../../_components/Header';
-import SearchList from '../_components/SearchList';
-import SearchPageFillter from '../_components/SearchPageFillter';
 import sty from '../_components/searchPage.module.css'
 
 
-export default function SearchPage() {
+export default function TicketPage() {
   return (
     <>
       <div className={sty.main_search_wrapper}>
         <Header />
-        <main className={sty.main_search_con}>
-          <SearchPageFillter/>
-          <SearchList/>
-        </main>
+
       </div>
       <Footer />
     </>
